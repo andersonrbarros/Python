64bcd2c756286e0615c7465b5c02b4799a3f3a5c
Anderson R. Barros <anderson.reis.barros@gmail.com>
2019-07-20 14:03:15 -0300

    Adicionando os arquivos...

---
+++ q4.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite nota 1:"))
+nota2 = float(input("Digite nota 1:"))
+nota3 = float(input("Digite nota 1:"))
+nota4 = float(input("Digite nota 1:"))
+
+media=(nota1+nota2+nota3+nota4)/4
+
+print("Média %.2f"%media)
---
+++ q5.py
@@ -0,0 +1,5 @@
+metros = float(input("Metros: "))
+
+cent = metros*100
+
+print("%.2f metros equivalem a %.2f centimetros"%(metros,cent))
